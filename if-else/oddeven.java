e26a5e164963c9ef0e090852610f95368a055317
Madhav Garg <sidharthgarg29082004@gmail.com>
2025-09-15 10:06:08 +0530

    if-else-switch conditional questions

---
+++ if-else/oddeven.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class oddeven {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = input.nextInt();
+        if(num%2==0){
+            System.out.print("Even");
+        }else{
+            System.out.print("Odd");
+        }
+    }
+}
